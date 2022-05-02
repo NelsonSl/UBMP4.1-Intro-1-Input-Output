7688fd83a29d42c32db8081ecfd950b60c3d3a21
nelsonsenglow@gmail.com <nelsonsenglow@gmail.com>
2022-05-02 18:15:32 +0000

    PlaySound

--- UBMP4-1-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
+++ UBMP4-1-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
@@ -1,72 +1,127 @@
 #include    "xc.h"              // Microchip XC8 compiler include file
 #include    "stdint.h"          // Include integer definitions
 #include    "stdbool.h"         // Include Boolean (true/false) definitions
- 
 #include    "UBMP410.h"         // Include UBMP4.1 constant and function definitions
- 
 // TODO Set linker ROM ranges to 'default,-0-7FF' under "Memory model" pull-down.
 // TODO Set linker code offset to '800' under "Additional options" pull-down.
- 
 // Program constant definitions
 const unsigned char maxCount = 50;
- 
 // Program variable definitions
 unsigned char SW3Count = 0;
 bool SW3Pressed = false;
  
-void playNote(int period, int durationCycles) {
-   for(int i = 0; i<durationCycles; i++) {
-       BEEPER = !BEEPER;
-       __delay_us(period);
-   }
+//  void rest( __delay_us)
+void playNote(int period, int durationCycles, bool isRest) {
+  for(int i = 0; i<durationCycles; i++)
+  {
+      if (!isRest) BEEPER = !BEEPER;
+      for(int j = 0; j < period; j++);
+  }
 }
  
-void playENote (){
-   playNote(E, 1000);
-}
+const int C = 329;
+const int D = 293;
+const int E = 261;
+const int G = 391;
+const int q = 500;
  
-const int C = 3822;
-const int D = 3412;
-const int E = 3033;
-const int G = 2551;
 int main(void)
 {
-   // Configure oscillator and I/O ports. These functions run once at start-up.
-   OSC_config();               // Configure internal oscillator for 48 MHz
-   UBMP4_config();             // Configure on-board UBMP4 I/O devices
-    
-   // Code in this while loop runs repeatedly.
-   while(1)
-       {
  
-    if(SW5 == 0 && SW4==1 && SW3==1) //c
-       {
-           playNote(E, 1000);
-           playNote(D, 1000);
-           playNote(C, 1000);
-           playNote(E, 1000);
-           playNote(C, 1000);
-           playNote(C, 1000);
-           playNote(C, 1000);
-           playNote(D, 1000);
-           playNote(D, 1000);
-           playNote(D, 1000);
-           playNote(E, 1000);
-           playNote(G, 1000);
-           playNote(G, 1000);
-           playNote(E, 1000);
-           playNote(D, 1000);
-           playNote(C, 1000);
-           playNote(D, 1000);
-          for (int i = 0; i < 3; i++) playENote();
+  OSC_config();              
+  UBMP4_config();            
+  
+  while(1)
+      {
+if(SW5 == 0 && SW4==1 && SW3==1)
+      {
+playNote(E, q, false);
+playNote(E, q, true);
  
-       }
+playNote(D, q, true);
+playNote(D, q, false);
  
+playNote(C, q, true);
+playNote(C, q, false);
+         
+playNote(D, q, true);
+playNote(D, q, false);
  
+playNote(E, q, true);
+playNote(E, q, false);
  
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(G, q, true);
+playNote(G, q, false);
+ 
+playNote(G, q, true);
+playNote(G, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(C, q, true);
+playNote(C, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(E, q, true);
+playNote(E, q, false);
+ 
+playNote(D, q, true);
+playNote(D, q, false);
+ 
+playNote(C, q, true);
+playNote(C, q, false);
+      }
+ 
+      if(SW3 == 0)
+      {
+      BEEPER == !BEEPER;
+      __delay_us (q);
+      }
 if(SW1 == 0)
-   {
-           RESET();
-   }
-       }
+  {
+          RESET();
+  }
+      }
 }
